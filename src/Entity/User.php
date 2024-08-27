338c927dce2893ae05665de3eedbcc5b9e60a8ae
NICOLAU Bastien <bastien.nicolau@univ-amu.fr>
2024-08-27 16:27:57 +0200

    Remove wallet ok

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,8 @@ class User
     private ?int $id;
     private array $wallets = [];
 
+    public const EXCEPTION_MESSAGE_CANNOT_REMOVE_LAST_WALLET = 'Cannot remove the last wallet';
+
     public function __construct()
     {
         $this->wallets[] = new Wallet();
@@ -20,6 +22,21 @@ class User
     public function addWallet(Wallet $walletToAdd): self
     {
         $this->wallets[] = $walletToAdd;
+
+        return $this;
+    }
+
+    public function removeWallet(Wallet $walletToRemove): self
+    {
+        if (count($this->wallets) === 1) {
+            throw new \Exception(self::EXCEPTION_MESSAGE_CANNOT_REMOVE_LAST_WALLET);
+        }
+
+        $key = array_search($walletToRemove, $this->wallets, true);
+
+        if ($key !== false) {
+            unset($this->wallets[$key]);
+        }
 
         return $this;
     }
--- tests/User/UserTest.php
+++ tests/User/UserTest.php
@@ -26,4 +26,29 @@ class UserTest extends TestCase
         $this->assertSame($nbWalletsBeforeAdd + 1, count($this->user->getWallets()));
         $this->assertSame($walletName, $this->user->getWallets()[count($this->user->getWallets()) - 1]->getName());
     }
+
+    public function testCanRemoveWallet(): void
+    {
+        $walletName = 'wallet1';
+        $walletToAdd = new Wallet($walletName);
+        $this->user->addWallet($walletToAdd);
+        $nbWalletsBeforeRemove = count($this->user->getWallets());
+
+        $this->user->removeWallet($walletToAdd);
+
+        $this->assertSame($nbWalletsBeforeRemove - 1, count($this->user->getWallets()));
+    }
+
+    public function testCannotRemoveLastWallet(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage(User::EXCEPTION_MESSAGE_CANNOT_REMOVE_LAST_WALLET);
+
+        $walletName = 'wallet1';
+        $walletToAdd = new Wallet($walletName);
+        $this->user->addWallet($walletToAdd);
+
+        $this->user->removeWallet($walletToAdd);
+        $this->user->removeWallet($walletToAdd);
+    }
 }
